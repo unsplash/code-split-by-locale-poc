80544d8947e27398181f5236b4fdc2bc39df0a1b
Oliver Joseph Ash <oliverjash@gmail.com>
2022-10-17 09:02:06 +0100

    Document

--- src/MyComponent/lang/index.js
+++ src/MyComponent/lang/index.js
@@ -4,7 +4,11 @@
 export const getTranslations = (lang) => {
     switch (lang) {
         case "en-US":
+            // Signal to the bundler that we want to bundle this module but we
+            // want to import it lazily (unlike `require`).
             import("./en-US");
+            // On the client-side: read the pre-loaded module
+            // On the server-side: load the module
             return __webpack_require__(require.resolveWeak("./en-US"));
         case "es-ES":
             import("./es-ES");
